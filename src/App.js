3b077b8a5ca9a2f8de33abc687109a804974881c
EMMAD Developer <emmadsadiq55@gmail.com>
2021-07-27 01:20:15 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
    <h2>hello world</h2>
+   </>
   );
 }
 
